0583b9afa0f7e56a15c7cf001d45c90f4c0f3066
Brian <brianccain@gmail.com>
2013-06-22 15:43:22 -0700

    (maint) Add more pages

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Here is my updated personal website
 
 JS authentication for blog?
 Simple blog
+  tabs on the left (/components) for blog entries
 Move old data to new website
 Webpage resume
   two col, one side with my name, photo, and contact
--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,3 @@
-$(document).ready(function() {$.get("menu.html", function(data) { $('#menu1').append(data); });});
+$(document).ready(function() {$.get("menu/menu.html", function(data) { $('#menu1').append(data); });});
+$(document).ready(function() {$.get("menu/menuab.html", function(data) { $('#menuab').append(data); });});
+$(document).ready(function() {$.get("menu/menuct.html", function(data) { $('#menuct').append(data); });});
